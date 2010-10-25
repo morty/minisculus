16d30b24ad96f63d69e4dc7751170b93cd15e111
Tom Mortimer-Jones <tom@morty.co.uk>
2010-10-25 22:06:04 +0100

    Answered question 5.

--- mark4.py
+++ mark4.py
@@ -29,3 +29,11 @@ if __name__ == '__main__':
     mark4 = MarkIV(7, 2)
     decoded = mark4.decode("""WZyDsL3u'0TfxP06RtSSF 'DbzhdyFIAu2 zF f5KE"SOQTNA8A"NCKPOKG5D9GSQE'M86IGFMKE6'K4pEVPK!bv83I""")
     send_answer(decoded, '/4baecf8ca3f98dc13eeecbac263cd3ed')
+
+    for i in range(10):
+        for j in range(10):
+            mark4 = MarkIV(i, j)
+            decoded = mark4.decode("""QT4e8MJYVhkls.27BL9,.MSqYSi'IUpAJKWg9Ul9p4o8oUoGy'ITd4d0AJVsLQp4kKJB2rz4dxfahwUa"Wa.MS!k4hs2yY3k8ymnla.MOTxJ6wBM7sC0srXmyAAMl9t"Wk4hs2yYTtH0vwUZp4a"WhB2u,o6.!8Zt"Wf,,eh5tk8WXv9UoM99w2Vr4!.xqA,5MSpWl9p4kJ2oUg'6evkEiQhC'd5d4k0qA'24nEqhtAQmy37il9p4o8vdoVr!xWSkEDn?,iZpw24kF"fhGJZMI8nkI""")
+            if 'FURLIN' in decoded and 'BUNKER' in decoded:
+                print decoded
+                print "%d, %d" % (i, j)
