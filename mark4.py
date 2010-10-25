ffbd1b81eacfc71feab22cde11e80436efd8e0b0
Tom Mortimer-Jones <tom@morty.co.uk>
2010-10-25 22:00:53 +0100

    Answered question 4.

--- mark4.py
+++ mark4.py
@@ -25,3 +25,7 @@ if __name__ == '__main__':
     mark4 = MarkIV(4, 7)
     encoded = mark4.encode('The white cliffs of Alghero are visible at night')
     send_answer(encoded, '/36d80eb0c50b49a509b49f2424e8c805')
+
+    mark4 = MarkIV(7, 2)
+    decoded = mark4.decode("""WZyDsL3u'0TfxP06RtSSF 'DbzhdyFIAu2 zF f5KE"SOQTNA8A"NCKPOKG5D9GSQE'M86IGFMKE6'K4pEVPK!bv83I""")
+    send_answer(decoded, '/4baecf8ca3f98dc13eeecbac263cd3ed')
